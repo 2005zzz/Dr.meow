9bd8b8d83449e701b809da0afdb09d5c92379fe9
王禹捷 <10932041@mail.hcsh.tp.edu.tw>
2025-11-02 22:11:09 +0800

    目前表單

--- Dr.meow/obj/Debug/net8.0/Dr.meow.AssemblyInfo.cs
+++ Dr.meow/obj/Debug/net8.0/Dr.meow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dr.meow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05fae21bd901f460f960016f11a417eeac7a900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70362490e63b0ff66ee2c873dc0a7648563fc211")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dr.meow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dr.meow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
