3ed32ae32f9f42b3b337acba1c521f8d8ea7591f
王禹捷 <10932041@mail.hcsh.tp.edu.tw>
2025-12-02 16:00:29 +0800

    feat: 完成卡片清單頁面

--- Dr.meow/Pages/Forms/FormsList.cshtml.cs
+++ Dr.meow/Pages/Forms/FormsList.cshtml.cs
@@ -1,12 +1,35 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Dr.meow.Data;
+using Dr.meow.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Dr.meow.Pages.Forms
 {
+    // 請確保此處的命名空間與您的專案結構一致
     public class FormsListModel : PageModel
     {
-        public void OnGet()
+        // 宣告資料庫上下文
+        private readonly DrMeowDbContext _context;
+
+        // 這是儲存從資料庫讀取出來的漏洞/變更清單的屬性
+        public IList<Vulnerability> VulnerabilityList { get; set; } = default!;
+
+        // 構造函式：透過依賴注入取得資料庫上下文
+        public FormsListModel(DrMeowDbContext context)
         {
+            _context = context;
+        }
+
+        // 處理 HTTP GET 請求
+        public async Task OnGetAsync()
+        {
+            // 從資料庫中讀取所有的 Vulnerability 紀錄
+            // 如果您的資料表名稱有變動，請在這裡修正
+            VulnerabilityList = await _context.Vulnerability
+                                                .OrderByDescending(v => v.Id) // 依照 Id 降冪排序，顯示最新的紀錄
+                                                .ToListAsync();
         }
     }
 }
